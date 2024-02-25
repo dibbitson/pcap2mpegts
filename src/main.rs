af6199b85f5970e498e778b6db79a300c7b10b88
David Ibbitson <dibbitson@anduril.com>
2024-02-25 11:31:00 -0800

    Revert "Merge pull request #1 from dibbitson/ps/rr/simplify__main__using_functional_programming_stuff"
    This reverts commit b4a9ac4966ced37f94d0bf6e854ebabde6447dfd, reversing
    changes made to e4b6c7b7d7508e99e06407fa7e7ce01a2320d150.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
-use pcap2mpegts::{config::Config, Converter};
+
+
+use std::process;
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    Config::build().and_then(Converter::run)
+fn main() -> Result<(), Box<dyn Error>>
+{
+    let config = match pcap2mpegts::config::Config::build() {
+        Err(e) => {
+            println!("Configuration error: {e}");
+            process::exit(1);
+        }
+        Ok(s) => { s }
+    };
+    
+    if let Err(e) = pcap2mpegts::Converter::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+ 
+    Ok(())
 }
